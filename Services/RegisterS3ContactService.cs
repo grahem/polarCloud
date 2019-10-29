b97e1f98f8a37f93d2b72df13c8ef175afed9c0d
grahem <grahem@gmail.com>
2019-10-29 11:37:24 -0700

    initial commit

---
+++ Services/RegisterS3ContactService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Site.ServiceModels;
+using System;
+
+namespace Site.Services
+{
+    
+    public class RegisterS3ContactService : IRegisterContactService
+    {
+
+        ContactHttpResponse IRegisterContactService.Register(Contact contact)
+        {
+            //call S3
+            contact.Id = Guid.NewGuid().ToString();
+            return new ContactHttpResponse() { Contact = contact, Date = DateTime.Now, Status = System.Net.HttpStatusCode.Created };
+        }
+
+    }
+
+    
+        
+}
+
+    
